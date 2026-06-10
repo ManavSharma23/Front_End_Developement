f226ee5978824acb7177d8d3eae0e9f39a202a8e
Manav Sharma <info.manavsharma2302@gmail.com>
2026-06-10 20:06:15 +0530

    added inventory function and inventory

--- Practice(Understanding Concept)/Codes/simple game/game.js
+++ Practice(Understanding Concept)/Codes/simple game/game.js
@@ -10,12 +10,11 @@ let fighting;
 
 let monster_health;
 
-let inventory = ["Water", "Knife"];
-
 
 const go_to_store = document.querySelector("#button1");
 const go_to_cave = document.querySelector("#button2");
 const fight_dragon = document.querySelector("#button3");
+const go_to_inventory = document.querySelector("#button4");
 
 const text = document.querySelector("#text");
 const xptext = document.querySelector("#xptext");
@@ -29,25 +28,41 @@ const monsterhealth = document.querySelector("#monsterhealth");
 const locations = [
     {
         name: "Town Square",
-        "Button Text": ["Go to Store", "Go to Cave", "Fight Dragon"],
-        "button_functions": [goStore, goCave, goFight],
+        "Button Text": ["Go to Store", "Go to Cave", "Fight Dragon", "Inventory"],
+        "button_functions": [goStore, goCave, goFight, goInventory],
         text: "You Are In the Town Square . "
     },
 
     {
         name: "store",
-        "Button Text": ["Buy 10 Health (10 Gold)", "Buy Weapons", "Go to Town Square"],
-        "button_functions": [buyHealth, buyWeapons, goTown],
+        "Button Text": ["Buy 10 Health (10 Gold)", "Buy Weapons", "Go to Town Square", "Inventory"],
+        "button_functions": [buyHealth, buyWeapons, goTown, goInventory],
         text: "Welcome To Dragonaise Summersville .\nThe Store of Enchantments and Honor. \n\nWhat would you like to buy?"
 
     },
 
     {
         name: "Cave",
-        "Button Text": ["Fight Slime", "Fight FangBeast", "Go To Town Square"],
-        "button_functions": [fightSlime, fightFangBeast, goTown],
+        "Button Text": ["Fight Slime", "Fight FangBeast", "Go To Town Square", "Inventory"],
+        "button_functions": [fightSlime, fightFangBeast, goTown, goInventory],
         text: "You Enter the Cave. You See some monsters !! \nWho are you going to fight ?",
 
+    }
+
+]
+
+let inventory = [
+    {
+        category: "edible",
+        name: "Water",
+        quantity: 1,
+    },
+
+    {
+        category: "weapon",
+        name: "Knife",
+        price: 0,
+        damage: 10,
     }
 
 ]
@@ -83,6 +98,7 @@ const Weapons = [
 go_to_store.onclick = goStore;
 go_to_cave.onclick = goCave;
 fight_dragon.onclick = goFight;
+go_to_inventory.onclick = goInventory;
 
 
 function update(locations) {
@@ -90,11 +106,13 @@ function update(locations) {
     go_to_store.innerText = locations["Button Text"][0];
     go_to_cave.innerText = locations["Button Text"][1];
     fight_dragon.innerText = locations["Button Text"][2];
+    go_to_inventory.innerText = locations["Button Text"][3];
     text.innerText = locations.text;
 
     go_to_store.onclick = locations["button_functions"][0];
     go_to_cave.onclick = locations["button_functions"][1];
     fight_dragon.onclick = locations["button_functions"][2];
+    go_to_inventory.onclick = locations["button_functions"][3];
 
 }
 
@@ -154,20 +172,46 @@ function buyHealth() {
 
 
 function buyWeapons() {
-    text.innerText = "Weapon list :\n";
+    text.innerText = "Weapon list :\n \n";
 
     for (let i = 0; i < Weapons.length; i++) {
-        text.innerText += "Weapon : " + Weapons[i].name + "Price : " + Weapons[i].price + "Damage : " + Weapons[i].damage + "\n"
+        text.innerText += (i + 1) + "." + "Weapon : " + Weapons[i].name + "\n"
+        text.innerText += " Price : " + Weapons[i].price + "\n"
+        text.innerText += " Damage : " + Weapons[i].damage + "\n"
         text.innerText += "\n"
 
+    }
+}
+function fightSlime() {
 
+}
+
+function fightFangBeast() {
+
+}
+
+function goInventory() {
+    console.log("Going to Inventory")
+    text.innerText = "Welcome To Your Inventory : \n \n"
+
+    text.innerText += "\n Category : Weapons \n \n"
+    let j = 1;
+    for (let i = 0; i < inventory.length; i++) {
+        if (inventory[i].category === "weapon") {
+            text.innerText += (j) + ".Name : " + inventory[i].name + "\n"
+            text.innerText += "  Damage : " + inventory[i].damage + "\n"
+            text.innerText += "  Price : " + inventory[i].price + "\n" + "\n"
+            j++;
+        }
+    }
+    let k = 1;
+    text.innerText += "\n Category : Edibles \n" + "\n"
+    for (let i = 0; i < inventory.length; i++) {
+        if (inventory[i].category === "edible") {
+            text.innerText += (k) + ".Name : " + inventory[i].name + "\n"
+            text.innerText += "  Quantity : " + inventory[i].quantity + "\n"
+            k++;
+        }
     }
 
-
-    function fightSlime() {
-
-    }
-
-    function fightFangBeast() {
-
-    }
+}
